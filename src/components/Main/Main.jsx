eb19466badcc8edc5c4519387b78ce14fe92c3f4
Zobaer <zobaerzahad.git@gmail.com>
2025-04-04 04:40:54 +0600

    some edit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -20,7 +20,7 @@ const Main = () => {
                 {!showResult 
                 ?<>
                     <div className="greet">
-                    <p><span>Hello, Sale.</span></p>
+                    <p><span>Abbe, Sale.</span></p>
                     <p>How can I help you today?</p>
                 </div>
                 <div className="cards">
